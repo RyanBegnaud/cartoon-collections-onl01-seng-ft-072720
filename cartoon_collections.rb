aa11c52b3b5d6db851259dd85cf275afc7caf841
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-30 17:00:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,14 @@ end
 
 def find_the_cheese(array)
   array.map do |cheese|
-    if array.include? == "cheddar"
+  if array.include?("cheddar")
+    return "cheddar"
+  elsif array.include?("gouda")
+    return "gouda"
+  elsif array.include?("camembert")
+    return "camembert"
+  else 
+    return nil
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
