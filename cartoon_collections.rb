d761393838cb76f1ed4182b9216f4568acd1e710
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-30 16:41:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,6 @@ def roll_call_dwarves(array)
   array.each_with_index do |name, index|
     puts "#{index+1}. #{name}"
   end
-end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|call| call.capitalize + "!"}
 end
@@ -15,4 +14,5 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+end
 end
