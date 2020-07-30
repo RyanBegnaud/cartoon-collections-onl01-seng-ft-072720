dd7e4f52a25600903c0b4d6a0417e26a4ced1c1d
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-30 16:44:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,12 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.each do |word|
+    if word.length > 4
+      true 
+    else 
+      false 
 end
 
 def find_the_cheese# code an argument here
