24566001f63eb26887a05450ed501c26b2fa113f
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-30 16:26:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,6 @@
 def roll_call_dwarves(array)
   array.each_with_index do |name|
     puts name
-  
-  # code an argument here
-  # Your code here
 end
 
 def summon_captain_planet# code an argument here
